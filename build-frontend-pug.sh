0328b0ff7945b7d4de1747bb230ab735654172c5
Heather Soron Kaminski <heather-work@soron.me>
2021-04-09 15:04:29 +0000

    Fix a typo in a build script, from JSON -> bash conversion

--- build-frontend-pug.sh
+++ build-frontend-pug.sh
@@ -8,5 +8,5 @@ set -e
 
 # couple edits using regexes, to make the compiled files play nice with Parcel
 # doing this via regex rather than a proper syntax-aware preprocessor is a bit brittle, but will probably be fine with our current build pipeline
-perl -pi -e 's/(function \\w+Template)/export $1/g' public/js/templates/*
+perl -pi -e 's/(function \w+Template)/export $1/g' public/js/templates/*
 perl -pi -e 's/require\(/global["require"](/g' public/js/templates/*
